b9723b727e6e962f9369e9dbaeb64fbf08c45cc2
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-12-31 01:14:32 +0530

    to string mongo url

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-     mongoose.connect(process.env.MONGO_URL, {
+     mongoose.connect(process.env.MONGO_URL.toString(), {
         useNewUrlParser: true,
         useUnifiedTopology: true,
      
