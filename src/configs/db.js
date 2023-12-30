14ad7a3e5300d1ad872f330370f203015c22262d
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-12-31 01:41:33 +0530

    connect3

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-     mongoose.connect(`${process.env.MONGO_URL}`, {
+     mongoose.connect("mongodb+srv://praveen7523017052:king979398@cluster0.zvofn6n.mongodb.net/", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
      
