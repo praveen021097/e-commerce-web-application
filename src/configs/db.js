77f11948b912284ee37b45fd6658cd8e540460d4
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2023-12-31 01:38:33 +0530

    connect2

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-     mongoose.connect(process.env.MONGO_URL, {
+     mongoose.connect(`${process.env.MONGO_URL}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
      
